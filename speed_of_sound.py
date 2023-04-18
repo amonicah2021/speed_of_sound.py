7ab91015a9a843361838cbb6d287c274a5b26cba
amonicah2021 <129651129+amonicah2021@users.noreply.github.com>
2023-04-18 13:45:48 +0300

    Create speed_of_sound.py

---
+++ speed_of_sound.py
@@ -0,0 +1,5 @@
+temperature = float(input("Введите температуру (в градусах Цельсия): "))
+
+speed_of_sound = 331.4 + (0.6 * temperature)
+
+print("Скорость звука при", temperature, "градусах Цельсия:", speed_of_sound, "м/с")
